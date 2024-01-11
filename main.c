f2f37be3dc85989756840b670c533b028d42869b
JunWei Song <sungboss2004@gmail.com>
2024-01-11 17:34:26 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,26 @@
 
 int main() {
     printf("Hello, World!\n");
-    int value = one_argu(1);
-    printf("Result is %d:",value);
+    int value1 = std_one_argu(1);
+    int value2 = cdecl_one_argu(1);
+    int value3 = fastcall_one_argu(1);
     return 0;
 }
 
-int __stdcall one_argu(int one){
-    printf("one argument func\n");
+int __stdcall std_one_argu(int one){
+    printf("stdcall one argument func\n");
+    int a = 2;
+    return one +a;
+}
+
+int __cdecl cdecl_one_argu(int one){
+    printf("cdecl one argument func\n");
+    int a = 2;
+    return one +a;
+}
+
+int __fastcall fastcall_one_argu(int one){
+    printf("fastcall one argument func\n");
     int a = 2;
     return one +a;
 }
